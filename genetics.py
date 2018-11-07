fda1c7315f9361409334e4c3d6c92660560586c0
Louis Warren <louis@lsw.nz>
2018-11-07 16:11:46 +1300

    Abstract out genetics

---
+++ genetics.py
@@ -0,0 +1,27 @@
+from heapq import nlargest
+
+def pool_iteration(pool, fitness, max_size, mutator, crosser, trace=0):
+    scored = []
+    for x in pool:
+        score = fitness(x)
+        if score > 0.5:
+            scored.append((x, score))
+    fittest = nlargest(max_size, scored, key=lambda x: x[1])
+    if trace:
+        for x, score in fittest[:trace]:
+            print(score, end='\t')
+        print()
+    if mutator:
+        for x, _ in fittest:
+            yield mutator(x)
+    if crosser:
+        for i, x_score in enumerate(fittest):
+            x, _ = x_score
+            for y, _ in fittest[i + 1:]
+                yield crosser(x, y)
+    for x, _ in fittest:
+        yield x
+
+def run_pool(pool, fitness, max_size, mutator, crosser, trace=0):
+    while True:
+        pool = pool_iteration(pool, fitness, max_size, mutator, crosser, trace)
